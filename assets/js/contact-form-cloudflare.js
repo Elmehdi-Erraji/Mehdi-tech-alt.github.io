70e3140f452fba4125e664b409e14ed33695845e
Mehdi <elmahdi20erraji@gmail.com>
2023-04-16 21:49:59 +0000

    Portfolio created

---
+++ assets/js/contact-form-cloudflare.js
@@ -0,0 +1,41 @@
+const TOKEN = '';
+const CHAT_ID = '5393516822';
+const requestHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
+    "Access-Control-Max-Age": "86400",
+}
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+    if (request.method == "OPTIONS") {
+        return new Response(null, {
+            headers: requestHeaders
+        })
+    } else {
+        const body = await request.json()
+        const {name, email, message} = body;
+
+        if (!name || !email || !message) {
+            return new Response('Please fill in all fields.', {
+                status: 200,
+            })
+        }
+        const response = await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
+            body: JSON.stringify({
+                chat_id: CHAT_ID,
+                text: `<b>New message recived.</b>\n\n<b>Name: </b> ${name}\n<b>Email: </b>${email}\n<b>Message: </b>${message}\n<b>IP: </b>${request.headers.get("cf-connecting-ip")}`,
+                parse_mode: 'HTML'
+            }),
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        const results = await response.json();
+        const responseMessage = results.ok ? 'Your message has been successfully sent.' : 'An error occurred while sending the message.'
+        return new Response(responseMessage, {status: 200, headers: requestHeaders})
+    }
+}
---
+++ readme.md
@@ -0,0 +1,45 @@
+# Responsive Personal Portfolio - Dev Chapter
+ - Real contact form connected to Telegram
+ - Dark and light version
+ - Load content on scroll,
+ - Only with HTML CSS & JavaScript
+ - Responsive design
+
+
+![](screenshot.jpg)
+
+
+### 🚀 Youtube video
+https://youtu.be/diIbwA-BAgo
+
+### 🔥 Demo
+https://portfolio-dev-chapter.vercel.app/
+
+### 📁 Assets
+<hr>
+
+#### Images
+https://unsplash.com
+
+##### Portfolio images
+- https://themefisher.com/products/promodise
+- https://themefisher.com/products/sleek
+- https://themefisher.com/products/dot
+- https://demo.themefisher.com/reporter/
+
+#### Icons
+https://www.figma.com/community/file/942053544758339202
+https://www.figma.com/community/file/931906394678748246
+
+#### JS Libraries
+- https://michalsnik.github.io/aos/
+- https://lusaxweb.github.io/Kursor/
+- https://swiperjs.com/
+- https://github.com/jhildenbiddle/css-vars-ponyfill
+- https://github.com/jonathantneal/svg4everybody
+- https://github.com/developit/unfetch
+#### Fonts
+- https://fonts.google.com/specimen/Inter
+- https://fonts.google.com/specimen/Plus+Jakarta+Sans
+#### CSS
+- https://meyerweb.com/eric/tools/css/reset/
