cfdcd0675229c3c6d63fb4991a4205986da6c800
Mehdi <elmahdi20erraji@gmail.com>
2023-07-04 22:13:21 +0100

    fix

--- construction/assets/js/main.js
+++ construction/assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: UpConstruction - v1.3.0
-* Template URL: https://bootstrapmade.com/upconstruction-bootstrap-construction-website-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
