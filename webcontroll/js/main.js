8ef3a22b4f9b15a9178c70c9d42f91d1e98c38fa
Mehdi <elmahdi20erraji@gmail.com>
2023-07-09 14:36:37 +0100

    fix

--- webcontroll/js/main.js
+++ webcontroll/js/main.js
@@ -59,20 +59,20 @@
         time: 2000
     });
     
-    
+    /*
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
+            $('.back-to-top').fadeIn('fast');
         } else {
-            $('.back-to-top').fadeOut('slow');
+            $('.back-to-top').fadeOut('fast');
         }
     });
     $('.back-to-top').click(function () {
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
     });
-
+*/
 
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
